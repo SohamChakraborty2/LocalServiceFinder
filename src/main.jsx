98f91625d78e2000a591ec9dd3f04e7c8a29deaa
Hitesh Adhikari <hiteshadhikari060603@gmail.com>
2023-12-16 23:19:29 +0530

    Linked login to application

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from './App.jsx'
-import Login from './components/Sign Up Page/login.jsx'
-import SignUp from './components/Sign Up Page/signup.jsx'
-import Provider_SignUp from './components/Sign Up Page/provider_signup.jsx'
-import Application from './components/Application/Application.jsx'
-import './index.css'
+import App from "./App.jsx";
+import Login from "./components/Sign Up Page/login.jsx";
+import SignUp from "./components/Sign Up Page/signup.jsx";
+import Provider_SignUp from "./components/Sign Up Page/provider_signup.jsx";
+import Application from "./components/Application/Application.jsx";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <App />,
   },
   {
     path: "application",
-    element: <Application />
+    element: <Application />,
   },
   {
     path: "login",
-    element: <Login />
+    element: <Login />,
   },
   {
     path: "signup",
-    element: <SignUp />
+    element: <SignUp />,
   },
   {
     path: "provider_signup",
-    element: <Provider_SignUp />
-  }
-])
-ReactDOM.createRoot(document.getElementById('root')).render(
+    element: <Provider_SignUp />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
